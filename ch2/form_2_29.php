e1041ebd1fa3b41d38eb5601ed9fdc7aec13e0ed
topsan <topsan2002@gmail.com>
2023-01-05 15:21:30 +0700

    learn ch2,5

---
+++ ch2/form_2_29.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>การสร้างฟอร์ม</title>
+</head>
+
+<body>
+    <!-- <h2>แบบฟอร์มบันทึกข้อมูลนักศึกษา</h2> -->
+  <form action="" method="post" name="form1" id="form1">
+Username : <input type="text" name="username" maxlength="10" /><br /> <br />
+Password : <input type="password" name="password" maxlength="6" /><br /> <br />
+ความคิดเห็น : <textarea cols="40" rows="5" name="comment" ></textarea><br />
+<br />
+<input type="submit" value="Submit Query">
+<input type="reset" >
+</body>
+
+</html>
